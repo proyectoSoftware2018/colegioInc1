8f328d38e54eda41e6118451809c37effb5c9cd3
proyectoSoftware2018 <43715815+proyectoSoftware2018@users.noreply.github.com>
2018-10-02 17:47:19 -0500

    probando nueva conexion aver

--- src/main/java/modelo/Conexion.java
+++ src/main/java/modelo/Conexion.java
@@ -12,7 +12,7 @@ public class Conexion {
     public static final String driver="com.mysql.jdbc.Driver";
     public static final String user="root";
     public static final String pass="root";
-    public static final String url = "jdbc:mysql://localhost:3306/colegio";
+    public static final String url = "jdbc:mysql://mysql:3306/colegio";
     
      /*
     //conexion en host
